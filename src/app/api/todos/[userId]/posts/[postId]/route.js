56a56f1c04146c82c0957f828af6682feffecb19
Arvind Kumar <thakurarvindkr10@gmail.com>
2023-09-28 08:16:47 +0530

    started new branch

--- src/app/api/todos/[userId]/posts/[postId]/route.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-export function GET(request, { params }) {
-  //   console.log(params.userId, params.postId);
-  const { userId, postId } = params;
-  console.log(userId, postId);
-
-  return NextResponse.json(params);
-}
